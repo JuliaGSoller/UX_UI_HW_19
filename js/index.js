29d75bdfeef057a1911cdea5526fbf282154ea79
JuliaGSoller <108694368+JuliaGSoller@users.noreply.github.com>
2023-01-26 10:39:12 -0500

    Added jQuery

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ console.log("this works");
 $("#DownloadResume").on("click", function(){
     console.log("SomeoneClicked");
 });
+$(".flexContainer div").mouseenter(function(){
+    $(this).css("width", "40%")
+});
+$(".flexContainer div").mouseleave(function(){
+    $(this).css("width", "33%")
+});
